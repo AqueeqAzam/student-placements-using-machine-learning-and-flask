dcfa17ccb03d9169443e1f032d272d349256f8ed
AQUEEQ AZAM <84491720+AqueeqAzam@users.noreply.github.com>
2024-06-16 02:49:37 +0530

    Update and rename ml-model.ipynb to ml-model.py

--- ml-model.py
+++ ml-model.py
@@ -1,4 +1,4 @@
-// copy this code and paste it in google colab or jupyter lab
+# copy this code and paste it in google colab or jupyter lab
 import pandas as pd
 import numpy as np
 
@@ -28,6 +28,6 @@ accuracy_score(y_test, SVC(kernel='rbf').fit(x_train, y_train).predict(x_test))
 svc = SVC(kernel = 'rbf')
 svc.fit(x_train, y_train)
 
-// run this comman in other cell
+# run this comman in other cell
 import pickle
 pickle.dump(svc, open('modle.pkl', 'wb'))
